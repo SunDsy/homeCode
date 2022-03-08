04effdd4c06d9b209a5ff6878e3739ec06574f37
SunDsy <before6hearts6dying6@gmail.com>
2022-03-08 16:21:37 +0200

    chenged logog of header

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return <header className='header'>
-        <img src='https://www.pikpng.com/pngl/m/59-596262_wolf-png-logo-for-free-download-on-wolf.png'></img>
+        <img src='https://i.playground.ru/i/wiki/8679/content/61zer2yv.png'></img>
     </header>
 }
 
