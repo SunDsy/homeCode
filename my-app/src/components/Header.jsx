95299728b0a7c44a6731044cc834526ae49ce81b
SunDsy <87501513+SunDsy@users.noreply.github.com>
2022-03-31 15:09:53 +0300

    Delete my-app directory

--- my-app/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import style from'./Header.module.css'
-
-const Header = () => {
-    return <header className={style.header}>
-        <img src='https://i.playground.ru/i/wiki/8679/content/61zer2yv.png'></img>
-    </header>
-}
-
-export default Header
